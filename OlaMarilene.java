45f7afae2615a3132108c4a3a64d182eab58290b
117110924 RAFAEL MATIAS DO AMARAL <rafaelma@lcc3-011.lcc.ufcg.edu.br>
2018-06-21 10:52:09 -0300

    teste

---
+++ OlaMarilene.java
@@ -0,0 +1,11 @@
+/**
+*Laboratório de Programação 2 - Lab 1
+*
+*@author Rafael Matias do Amaral - 117110924
+*/
+
+public class OlaMarilene {
+    public static void main(String[] args) {
+        System.out.println("Hello Dirlididi!");
+    }
+}
